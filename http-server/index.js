0d11a3f762611a1cb3c95e6b91737867e243f4f1
Mahammadruwaid Golandaj <mdruwaid63@gmail.com>
2025-05-21 20:42:05 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const path = require("path");
 const minimist = require("minimist");
 
-const app = express();
 const args = minimist(process.argv.slice(2));
 const port = args.port || 3000;
 
-// Serve static files (e.g., project.html)
+const app = express();
+
+// Serve static files from the current directory
 app.use(express.static(path.join(__dirname)));
 
-// Route to serve registration.html
+// Route for /registration explicitly
 app.get("/registration", (req, res) => {
   res.sendFile(path.join(__dirname, "registration.html"));
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
