95019accbb7c80bbb9e2bcfd5e9def418f6f6763
mahammadruwaid golandaj <mdruwaid63@gmail.com>
2025-05-21 19:16:24 +0530

    final commit for error

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,1 +1,9 @@
-console.log("Hello GitHub!");
+const fs = require("fs");
+fs.writeFile(
+  "sample.txt",
+  "Hello World. Welcome to Node.js File System module.",
+  (err) => {
+    if (err) throw err;
+    console.log("File created!");
+  }
+);
