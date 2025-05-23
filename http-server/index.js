1faa392ef7243d4c179de4a7a74bd5c8d94f2af9
mahammadruwaid golandaj <mdruwaid63@gmail.com>
2025-05-23 11:43:59 +0530

    final commit made new public foldere

--- http-server/index.js
+++ http-server/index.js
@@ -3,17 +3,28 @@ const path = require("path");
 const minimist = require("minimist");
 
 const args = minimist(process.argv.slice(2));
-const port = args.port || 5000;
+const port = args._[0] || 5000; // Use positional arg, not --port
 
 const app = express();
 
-// Serve all static files in the folder (home.html, project.html, etc.)
-app.use(express.static(path.join(__dirname)));
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, "public")));
 
-// Explicitly define /registration route
+// Route for home page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "home.html"));
+  res.sendFile(path.join(__dirname, "public", "home.html"));
 });
+
+// Route for project page
+app.get("/project", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "project.html"));
+});
+
+// Route for registration page
+app.get("/registration", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "registration.html")); // Corrected spelling
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
